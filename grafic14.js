39c38bb21d43747ebbd6249d5bd8bd0bdfc5cddd
Iferrero <ignacio.ferrero@uab.cat>
2022-07-05 07:56:44 +0200

    changes grafic14

--- grafic14.js
+++ grafic14.js
@@ -1,26 +1,32 @@
-function grafic14(workbook) {       
-    var rowObject = XLSX.utils.sheet_to_json(workbook.Sheets['Dashboard (3)'], {range: "EI4:EJ9"});
-    //delete rowObject[1]
-    var labels = rowObject.map(function(e) {
-        return e["Any"];
+function grafic14() {       
+
+    d3.csv('data/socioeconomics/atur_2021_2005.csv').then(function(data){
+        atur = data;
+        filtre_atur = atur;
+        filtre_atur = atur.filter(element => element.nommuni == "AMB");
+        pintar14();
+    });
+}
+
+function pintar14() {       
+
+    data_atur = filtre_atur.map(function(e) {
+        return  [e.atur_2005,e.atur_2006,e.atur_2007,e.atur_2008,e.atur_2009,e.atur_2010,e.atur_2011,e.atur_2012,e.atur_2013,e.atur_2014,e.atur_2015,e.atur_2016,e.atur_2017,e.atur_2018,e.atur_2019,e.atur_2020,e.atur_2021];
      });
 
-    var dades = rowObject.map(function(e) {
-        return e["aturs"];
-     });
 
 
 
     const ctx = document.getElementById('myChart14').getContext('2d');
-    const myChart = new Chart(ctx, {
+    myChart14 = new Chart(ctx, {
         
         data: {
-            labels: labels,
+            labels: ["2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021"],
             datasets: [
                 {
                     type: 'bar',
                     //label: "Persones a l'atur",
-                    data: dades,
+                    data: data_atur[0],
                     backgroundColor: 'rgba(192,0,0,1)',
                 }
             ]
